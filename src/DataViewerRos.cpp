935cbf0f93338e5508b82aa62067e93fbeb560f8
NekoSaiKou <ricky0711.yang@gmail.com>
2022-03-01 13:57:50 +0800

    Feat: Modify viewer for not only this dataset use

--- src/DataViewerRos.cpp
+++ src/DataViewerRos.cpp
@@ -25,36 +25,100 @@ using namespace std;
 class DataGrabber
 {
 public:
-    DataGrabber(ros::NodeHandle &nh):nh_(nh)
-    {
-        scan_pub_ = nh_.advertise<sensor_msgs::LaserScan>("/RobotAtHome/Scan", 1, true);
-        pointcloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/RobotAtHome/PointCloud", 1, true);
-    }
-
+    DataGrabber(const string &strSettingsFile);
     void GrabData(const sensor_msgs::ImageConstPtr &msgRGB, const sensor_msgs::ImageConstPtr &msgD, const sensor_msgs::LaserScanConstPtr &msgScan);
-    void PublishTF();
     sensor_msgs::PointCloud2 GeneratePointCloud(const cv::Mat &im, const cv::Mat &imD);
     sensor_msgs::LaserScan GenerateScanMsg(const sensor_msgs::LaserScanConstPtr &msgScan);
+    void PublishTF();
+
 private:
-    ros::NodeHandle nh_;
+    cv::Mat mTlc;
+    float fx, fy, cx, cy, invfx, invfy;
+    float DepthMapFactor;
+
+private:
+    ros::NodeHandle nh;
     ros::Publisher scan_pub_;
     ros::Publisher pointcloud_pub_;
 
-public:
-    static long nData;
+    message_filters::Subscriber<sensor_msgs::Image> *rgb_sub_;
+    message_filters::Subscriber<sensor_msgs::Image> *depth_sub_;
+    message_filters::Subscriber<sensor_msgs::LaserScan> *scan_sub_;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::LaserScan> sync_pol;
+    message_filters::Synchronizer<sync_pol> *sync_;
+
+private:
+    string mstrLaserTopic;
+    string mstrRGBTopic;
+    string mstrDepthTopic;
+
+    string mstrPubLaserFrame;
+    string mstrPubLaserTopic;
+    string mstrPubCloudFrame;
+    string mstrPubCloudTopic;
 };
-long DataGrabber::nData = 0;
+
+DataGrabber::DataGrabber(const string &strSettingsFile)
+{    
+    cv::FileStorage fSettings(strSettingsFile, cv::FileStorage::READ);
+    mstrLaserTopic = string(fSettings["ROS.LaserTopic"]);
+    mstrRGBTopic = string(fSettings["ROS.CameraTopic"]);
+    mstrDepthTopic = string(fSettings["ROS.DepthTopic"]);
+    mstrPubLaserFrame = string(fSettings["APP.PubLaserFrame"]);
+    mstrPubLaserTopic = string(fSettings["APP.PubLaserTopic"]);
+    mstrPubCloudFrame = string(fSettings["APP.PubCloudFrame"]);
+    mstrPubCloudTopic = string(fSettings["APP.PubCloudTopic"]);
+
+    // Subscriber Setting
+    rgb_sub_ = new message_filters::Subscriber<sensor_msgs::Image>(nh, mstrRGBTopic, 1);
+    depth_sub_ = new message_filters::Subscriber<sensor_msgs::Image>(nh, mstrDepthTopic, 1);
+    scan_sub_ = new message_filters::Subscriber<sensor_msgs::LaserScan>(nh, mstrLaserTopic, 1);
+    sync_ = new message_filters::Synchronizer<sync_pol>(sync_pol(10), *rgb_sub_, *depth_sub_, *scan_sub_);
+    sync_->registerCallback(boost::bind(&DataGrabber::GrabData, this, _1, _2, _3));
+
+    cout << "[SLAM] Listen to topic " << mstrLaserTopic << endl;
+    cout << "[SLAM] Listen to topic " << mstrRGBTopic << endl;
+    cout << "[SLAM] Listen to topic " << mstrDepthTopic << endl;
+
+    // Transformation Setting
+    float tCLtx = fSettings["Sensor.tCLtx"];
+    float tCLty = fSettings["Sensor.tCLty"];
+    float tCLtz = fSettings["Sensor.tCLtz"];
+    float tCLqx = fSettings["Sensor.tCLqx"];
+    float tCLqy = fSettings["Sensor.tCLqy"];
+    float tCLqz = fSettings["Sensor.tCLqz"];
+    float tCLqw = fSettings["Sensor.tCLqw"];
+
+    fx = fSettings["Camera.fx"];
+    fy = fSettings["Camera.fy"];
+    cx = fSettings["Camera.cx"];
+    cy = fSettings["Camera.cy"];
+    invfx = 1/fx;
+    invfy = 1/fy;
+
+    DepthMapFactor = fSettings["DepthMapFactor"];
+    if(fabs(DepthMapFactor)<1e-5)
+        DepthMapFactor=1;
+    else
+        DepthMapFactor = 1.0f/DepthMapFactor;
+
+    // Eigen::Matrix<float,3,3> rCL = Eigen::Quaternionf(tCLqw, tCLqx, tCLqy, tCLqz).toRotationMatrix();
+    // Eigen::Vector3f tCL(tCLtx, tCLty, tCLtz);
+
+    // Publisher Setting
+    scan_pub_ = nh.advertise<sensor_msgs::LaserScan>(mstrPubLaserTopic, 1, true);
+    pointcloud_pub_ = nh.advertise<sensor_msgs::PointCloud2>(mstrPubCloudTopic, 1, true);
+}
 
 void DataGrabber::GrabData(const sensor_msgs::ImageConstPtr &msgRGB, const sensor_msgs::ImageConstPtr &msgD, const sensor_msgs::LaserScanConstPtr &msgScan)
 {
     ros::Duration tstamp_diff(msgRGB->header.stamp - msgScan->header.stamp);
     double time_difference = abs(tstamp_diff.toSec());
-    if (time_difference > 0.1)
-    {
-        return;
-    }
-    DataGrabber::nData++;
-    std::cout << "[ROS mixed] New Set Get! n:" << nData << " Time difference:" << time_difference << std::endl;
+
+    // if (time_difference > 0.1)
+    // {
+    //     return;
+    // }
 
     cv_bridge::CvImageConstPtr cv_ptrRGB;
     try
@@ -78,15 +142,12 @@ void DataGrabber::GrabData(const sensor_msgs::ImageConstPtr &msgRGB, const senso
         return;
     }
 
-    std::cout << "Generate CLOUD topic" << std::endl;
     sensor_msgs::PointCloud2 RGBDcloud = GeneratePointCloud(cv_ptrRGB->image, cv_ptrD->image);
-    std::cout << "Generate SCAN topic" << std::endl;
     sensor_msgs::LaserScan SCAN = GenerateScanMsg(msgScan);
     PublishTF();
     pointcloud_pub_.publish(RGBDcloud);
     scan_pub_.publish(SCAN);
 
-    std::cout << std::endl;
 }
 
 void DataGrabber::PublishTF()
@@ -94,13 +155,13 @@ void DataGrabber::PublishTF()
     tf2::Matrix3x3 tf_laser_rotation(1, 0, 0,
                                      0, 1, 0,
                                      0, 0, 1);
-    tf2::Vector3 tf_laser_translation(0.205, 0, 0.31);
+    tf2::Vector3 tf_laser_translation(0, 0, 0);
     tf2::Transform tf_laser_to_base(tf_laser_rotation, tf_laser_translation);
 
     tf2::Matrix3x3 tf_camera_rotation( 0,  0, 1,
                                       -1,  0, 0,
                                        0, -1, 0);
-    tf2::Vector3 tf_camera_translation(0.285, 0, 1.045);
+    tf2::Vector3 tf_camera_translation(0, 0, 0);
     tf2::Transform tf_camera_to_base(tf_camera_rotation, tf_camera_translation);
 
     static tf2_ros::TransformBroadcaster tf_broadcaster;
@@ -109,28 +170,25 @@ void DataGrabber::PublishTF()
     tf2::Stamped<tf2::Transform> tf_map2laser_stamped;
     tf_map2laser_stamped = tf2::Stamped<tf2::Transform>(tf_laser_to_base, ros::Time::now(), "RobotAtHome_Base");
     geometry_msgs::TransformStamped msg_lidar = tf2::toMsg(tf_map2laser_stamped);
-    msg_lidar.child_frame_id = "RobotAtHome_Laser";
+    msg_lidar.child_frame_id = mstrPubLaserFrame;
     tf_broadcaster.sendTransform(msg_lidar);
 
     // Make Camera message
     tf2::Stamped<tf2::Transform> tf_map2camera_stamped;
     tf_map2camera_stamped = tf2::Stamped<tf2::Transform>(tf_camera_to_base, ros::Time::now(), "RobotAtHome_Base");
     geometry_msgs::TransformStamped msg_camera = tf2::toMsg(tf_map2camera_stamped);
-    msg_camera.child_frame_id = "RobotAtHome_Camera";
+    msg_camera.child_frame_id = mstrPubCloudFrame;
     tf_broadcaster.sendTransform(msg_camera);
 }
+
 
 sensor_msgs::PointCloud2 DataGrabber::GeneratePointCloud(const cv::Mat &im, const cv::Mat &imD)
 {
     cv::Mat depthmap = imD;
     // Data Preprocess
-    float mDepthMapFactor = 6553.5;
-    if(fabs(mDepthMapFactor)<1e-5)
-        mDepthMapFactor=1;
-    else
-        mDepthMapFactor = 1.0f/mDepthMapFactor;
-    if((fabs(mDepthMapFactor-1.0f)>1e-5) || depthmap.type()!=CV_32F)
-    depthmap.convertTo(depthmap,CV_32F,mDepthMapFactor);
+
+    if((fabs(DepthMapFactor-1.0f)>1e-5) || depthmap.type()!=CV_32F)
+    depthmap.convertTo(depthmap,CV_32F,DepthMapFactor);
 
     // Count Valid Data
     int num_data = 0;
@@ -146,21 +204,13 @@ sensor_msgs::PointCloud2 DataGrabber::GeneratePointCloud(const cv::Mat &im, cons
         }
     }
 
-    // Intrinsic
-    float fx = 271.36999;
-    float invfx = 1/fx;
-    float fy = 286.441384;
-    float invfy = 1/fy;
-    float cx = 120.0802295;
-    float cy = 157.3245865;
-
     // Generate Point Cloud
     sensor_msgs::PointCloud2 cloud;
 
     const int num_channels = 3; // x y z
     const int min_observations_per_point_ = 2;
     cloud.header.stamp = ros::Time::now();
-    cloud.header.frame_id = "RobotAtHome_Camera";
+    cloud.header.frame_id = mstrPubCloudFrame;
     cloud.height = 1;
     cloud.width = num_data;
     cloud.is_bigendian = false;
@@ -210,7 +260,7 @@ sensor_msgs::PointCloud2 DataGrabber::GeneratePointCloud(const cv::Mat &im, cons
 sensor_msgs::LaserScan DataGrabber::GenerateScanMsg(const sensor_msgs::LaserScanConstPtr &msgScan)
 {
     sensor_msgs::LaserScan SCAN;
-    SCAN.header.frame_id = string("RobotAtHome_Laser");
+    SCAN.header.frame_id = mstrPubLaserFrame;
     SCAN.header.stamp = ros::Time::now();
     SCAN.angle_min = msgScan->angle_min;
     SCAN.angle_max = msgScan->angle_max;
@@ -225,26 +275,25 @@ sensor_msgs::LaserScan DataGrabber::GenerateScanMsg(const sensor_msgs::LaserScan
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "RobotAtHome_ROS_Viewer");
+    ros::init(argc, argv, "RGBD");
     ros::start();
 
-    string strLaserTopic = "/scan";
-    string strRGBTopic = "/camera/rgb";
-    string strDepthTopic = "/camera/depth";
+    if (argc != 2)
+    {
+        cerr << endl
+             << "Usage: ./Example/ROS_Mixed/ros_mixed path_to_settings" << endl;
+        ros::shutdown();
+        return 1;
+    }
 
-    cout << "[RobotAtHome ROS Viewer] Listen to topic " << strLaserTopic << endl;
-    cout << "[RobotAtHome ROS Viewer] Listen to topic " << strRGBTopic << endl;
-    cout << "[RobotAtHome ROS Viewer] Listen to topic " << strDepthTopic << endl;
-    
-    ros::NodeHandle nh;
+    cv::FileStorage fSettings(argv[1], cv::FileStorage::READ);
+    if (!fSettings.isOpened())
+    {
+        cerr << "Failed to open settings file at: " << argv[2] << endl;
+        return -1;
+    }
 
-    DataGrabber igb(nh);
-    message_filters::Subscriber<sensor_msgs::Image> rgb_sub(nh, strRGBTopic, 1);
-    message_filters::Subscriber<sensor_msgs::Image> depth_sub(nh, strDepthTopic, 1);
-    message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub(nh, strLaserTopic, 1);
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::LaserScan> sync_pol;
-    message_filters::Synchronizer<sync_pol> sync(sync_pol(1), rgb_sub, depth_sub, scan_sub);
-    sync.registerCallback(boost::bind(&DataGrabber::GrabData, &igb, _1, _2, _3));
+    DataGrabber igb(argv[1]);
 
     ros::spin();
 
